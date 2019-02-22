8f080e437b60ab6db57f54c23e256b27aab148c5
Suvendu Mandal <m_andal2002@yahoo.co.in>
2019-02-22 23:12:10 +0000

    2nd commit

--- src/main/java/com/microservice/user/registration/UserRegistrationController.java
+++ src/main/java/com/microservice/user/registration/UserRegistrationController.java
@@ -9,9 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservice.user.model.UserModel;
+
+
+
 @RestController
 public class UserRegistrationController {
 	
-	
+	   @RequestMapping(value = "/CreateUser", method = RequestMethod.POST)
+	   public ResponseEntity<Object> createUser(@RequestBody UserModel user) {
+	      productRepo.put(product.getId(), product);
+	      return new ResponseEntity<>("Products are created successfully", HttpStatus.CREATED);
+	   }
 
 }
